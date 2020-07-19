1302efab1a09483ece1635e23f2bb9458c0a84ca
Ian Dorsey <ian.dorsey@gmail.com>
2020-07-19 16:25:47 -0700

    Mostly finished with main.py

--- geokeys/__main__.py
+++ geokeys/__main__.py
@@ -1,4 +1,10 @@
 import csv
+import sys
+
+from tools import generate_key
+
+class DoubleKeyError(Exception):
+    pass
 
 def readcsv(csvfile):
     '''Load rows from the CSV file'''
@@ -16,8 +22,29 @@ def merge(args):
     source_csv = readcsv(args.source_csv)
     target_csv = readcsv(args.target_csv)
 
-    source_geo_names = list(map(lambda x: x[0], source_csv))
-    target_geo_names = list(map(lambda x: x[0], target_csv))
+    source_geo_names = list(map(lambda x,y: (x, y[0]), enumerate(source_csv)))
+    target_geo_names = list(map(lambda x,y: (x, y[0]), enumerate(target_csv)))
+
+    source_keys_and_geo_names = list(map(lambda x,y: (x, generate_key(y), y), source_geo_names))
+    target_keys_and_geo_names = list(map(lambda x,y: (x, generate_key(y), y), target_geo_names))
+
+    matches = []
+
+    for source_id, source_key, geo_name in source_keys_and_geo_names:
+        these_matches = []
+        for target_id, target_key, geo_name in target_keys_and_geo_names:
+            if source_key == target_key:
+                these_matches.append((source_id, target_id))
+            
+        if len(these_matches) > 1:
+            raise DoubleKeyError("Key relationship not one-to-one.")
+        elif len(these_matches) == 1:
+            matches.append(these_matches[0])
+
+    csvwriter = csv.writer()
+
+    for source_id, target_id in match:
+        csvwriter.writerow(source_csv[source_id] + target_csv[target_id])
 
 ###############################################################################
 # Argument parsing with argparse
